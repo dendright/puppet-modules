2abda1f6c963e7328befb5afc77ab00565dd81d9
jriches <jriches@lunar.(none)>
2012-01-11 14:20:45 +0000

    Fully qualified path for 'psql'

--- postgresql/manifests/dbcreate.pp
+++ postgresql/manifests/dbcreate.pp
@@ -4,7 +4,7 @@
 define postgresql::dbcreate ($role , $password='' , $conntype="host" , $address='127.0.0.1/32' , $auth_method="md5" , $auth_options="" , $usertype="nosuperuser") {
     exec { "role_$name":
         user    => "${postgresql::params::processuser}",
-        unless  => "echo \\\\dg | psql | grep $name 2>/dev/null",
+        unless  => "echo \\\\dg | /usr/bin/psql | grep $name 2>/dev/null",
         command => "echo \"create role $name $usertype nocreatedb nocreaterole noinherit nologin ; create role $role nosuperuser nocreatedb nocreaterole noinherit login encrypted password '$password'; grant $name to $role; create database $name with owner=$role;\" | /usr/bin/psql",
         require => Service["postgresql"],
     }
